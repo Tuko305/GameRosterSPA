429d7729c40d51cc0cede449f4c0935c65cdbebc
Hugues Bonnaure <hugues.bonnaure@gmail.com>
2020-05-21 12:55:29 +0100

    trying to commit some files which wont come

---
+++ app/controllers/renderPost.js
@@ -0,0 +1,9 @@
+import {postPlayer} from '../services/api.js'
+import renderPost from '../views/post.js'
+
+export default function(){
+    const playerName = window.location.hash.split('?pname=')[1].split('.')[0];
+    const charName = window.location.hash.split('cname=')[1];
+    
+    postPlayer(playerName, charName).then(renderPost);
+}
---
+++ app/views/post.js
@@ -0,0 +1,16 @@
+export default function renderPlayer({ name, charName, guildName, pvpKillFame, pvpRatio}){
+   window.location.hash = "#roster";
+    /*$('#app')
+        .empty()
+            .append($('<h3>').text('Added to data base: if fields are empty the Albion API might not have responded, resubmit your post please.'))
+            .append($('<h1>').text(`Player Name: ${name}`))
+            .append($('<br>'))
+            .append($('<h2>').text(`Character Name: ${charName}`))
+            .append($('<h2>').text(`Guild Name: ${guildName}`))
+            .append($('<h2>').text(`PVP fame: ${pvpKillFame}`))
+            .append($('<h2>').text(`PVP Ratio: ${pvpRatio}`))
+            .append($('<h2>').text('Back to Roster').click(function(){
+                window.location.hash = "#roster";
+            }));
+            */
+};
