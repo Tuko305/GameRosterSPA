93eed1f3ac718017b265fd01a65210e6befcf2f5
Hugues Bonnaure <hugues.bonnaure@gmail.com>
2020-05-21 19:54:31 +0100

    added css elements and cleaned up code to make it work now that id was removed from model

--- app/controllers/renderPost.js
+++ app/controllers/renderPost.js
@@ -3,7 +3,7 @@ import renderPost from '../views/post.js'
 
 export default function(){
     const playerName = window.location.hash.split('?pname=')[1].split('.')[0];
-    const charName = window.location.hash.split('cname=')[1];
+    const charName = window.location.hash.split('.cname=')[1];
     
     postPlayer(playerName, charName).then(renderPost);
 }
--- app/views/roster.js
+++ app/views/roster.js
@@ -2,9 +2,9 @@ export default function renderRoster(results){
     console.log(results);
     $('#app')
         .empty()
-            .append($('<h1>').text('Roster'))
+            .append($('<h1 class="main">').text('Roster'))
             .append($('<br>'))
-            .append($('<h2>').text('Add new player to output'))
+            .append($('<h2>').text('Add new player to add to the database'))
             
             $('#app').append($('<br>'));
             var heyo = $('<form id="heyo">')
@@ -21,19 +21,18 @@ export default function renderRoster(results){
                 .append($('<button>').text('Submit Request').click(function(){
                     window.location.hash = `#post?pname=${pname.value}.cname=${cname.value}`;
                 }))
+                .append('<br>')
+                .append('<br>')
                 
             $('#app').append(heyo).append(heyo2);
 
 
             
-            results.forEach(player => {
-                if(player.pvpKillFame > 0){               
+            results.forEach(player => {              
                 $('#app').append($('<h2>').text(`${player.name}`).click(function(){
-                    window.location.hash = `#player/${player.id}`
+                    window.location.hash = `#player/${player.name}`
                 }));
-            }
-                
-            });
+           });
             
             function labeling(labelName, playerName){
                 return $(`<label for=${labelName}>`).text(playerName)
